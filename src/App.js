3fc1f6e0dae0a51a9b949b090fc2a454384f7a7a
Dian Sherman <slayer_barrett_@hotmail.com>
2023-07-24 04:00:13 -0700

    added a babel dependency

--- src/App.js
+++ src/App.js
@@ -2,74 +2,20 @@ import react from "react";
 import Home from "./pages/Home";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Profile from "./pages/PetPage";
-import Navbar from "./components/NavBar";
+import Navigation from "./components/NavBar";
 import Container from 'react-bootstrap/Container';
 import './App.css';
 
 
 function App() {
-  // const [userId, setUserId] = useState(0)
-  // const [userEmail, setUserEmail] = useState("")
-  // const [isLoggedIn, setIsLoggedIn] = useState(false)
-  // const [token, setToken] = useState("")
-
-  // useEffect(()=>{
-  //   const storedToken = localStorage.getItem("token")
-  //   if(storedToken){
-  //     console.log(storedToken)
-  //     API.getUserFromToken(storedToken).then(data=>{
-  //       if(data.user){
-  //         console.log(data)
-  //         setToken(storedToken)
-  //         setIsLoggedIn(true)
-  //         setUserId(data.user.id)
-  //         setUserEmail(data.user.email)
-  //       }
-  //     })
-  //   } else {
-  //     console.log('no stored token')
-  //   }
-  // },[])
-
-  // const handleLoginSubmit = userObj=>{
-  //   API.login(userObj).then(data=>{
-  //     console.log(data);
-  //     if(data.token){
-  //       setUserId(data.user.id)
-  //       setToken(data.token)
-  //       setIsLoggedIn(true)
-  //       setUserEmail(data.user.email)
-  //       localStorage.setItem("token",data.token)
-  //     }
-  //   })
-  // }
-  // const handleSignupSubmit = userObj=>{
-  //   API.signup(userObj).then(data=>{
-  //     console.log(data);
-  //     if(data.token){
-  //       setUserId(data.user.id)
-  //       setToken(data.token)
-  //       setIsLoggedIn(true)
-  //       setUserEmail(data.user.email)
-  //       localStorage.setItem("token",data.token)
-  //     }
-  //   })
-  // }
-  // const handleLogout = ()=>{
-  //   localStorage.removeItem("token");
-  //   setIsLoggedIn(false);
-  //   setUserId(0);
-  //   setToken("");
-  //   setUserEmail("")
-  // }
   return (
     <div className="App">
       <Container>
         <h1>I Love Pets!</h1>
         <h2>Petsitting made easy</h2>
       </Container>
+        <Navigation/>
       <Router>
-        <Navbar/>
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/profile" element={<Profile/>}/>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,14 +3,13 @@ import React from 'react'
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
 
-// import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function Navigation() {
     return (
         <Container>
             <Nav variant="pills" className="me-auto">
-                <Nav.Link href="/" >Home</Nav.Link>
-                <Nav.Link href="profile" >Profile</Nav.Link>
+                <Nav.Link href="/" >Home!</Nav.Link>
+                <Nav.Link href="profile" >Profile!</Nav.Link>
 
             </Nav>
 
@@ -20,17 +19,3 @@ function Navigation() {
 }
 
 export default Navigation;
-// export default function Navbar() {
-//     const navigate = useNavigate()
-//     // const logoutFunc = ()=>{
-//     //     props.handleLogout()
-//     //     navigate("/login")
-//     //   }
-//       return (
-//         <div>
-//             <Link to ="/">Home</Link>
-//             <Link to ="/Profile">Profile</Link>
-
-//         </div>
-//     )
-// }
